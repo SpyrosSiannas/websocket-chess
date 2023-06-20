f2e285be773319eee89be888d8b6c3e07607a748
SpyrosSiannas <ssiannas@gmail.com>
2023-06-20 20:45:56 +0300

    Add websockets

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ssiannas/code/websockets-cpp
-# Build directory: /home/ssiannas/code/websockets-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

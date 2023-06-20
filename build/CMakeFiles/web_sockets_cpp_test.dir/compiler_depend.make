f722b48a3b010cd907ea2451c716728188e2ec84
SpyrosSiannas <ssiannas@gmail.com>
2023-06-20 20:59:20 +0300

    Remove submodules

---
+++ build/CMakeFiles/web_sockets_cpp_test.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for web_sockets_cpp_test.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ssiannas/code/websockets-cpp
+# Build directory: /home/ssiannas/code/websockets-cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
